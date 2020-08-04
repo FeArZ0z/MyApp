948bcb422aa6622831a0b883b9091de1a0bcb89c
FeArZ0z <max.spiesmaier@aon.at>
2020-08-04 17:24:56 +0200

    idk

--- app/src/main/java/com/example/secondtry/ui/decks/EditDeck.java
+++ app/src/main/java/com/example/secondtry/ui/decks/EditDeck.java
@@ -3,7 +3,10 @@ package com.example.secondtry.ui.decks;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,27 +14,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.secondtry.R;
 
+import com.example.secondtry.ui.decks.AddDeck;
+
 import org.w3c.dom.Text;
 
 
 public class EditDeck extends AppCompatActivity {
 
     TextView deckName;
-
+    Button backbutton;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.deck_create_deck);
         String deckName = AddDeck.inputDeckName;
 
+
+        Button backbutton = (Button) findViewById(R.id.back_add_deck);
+        backbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                goBack();
+            }
+        });
+
+        Button addButton = (Button) findViewById(R.id.addQuestion);
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addCard();
+            }
+        });
+
         TextView deckNameTitle = (TextView) findViewById(R.id.deckName);
-        Toast.makeText(getApplicationContext(), "input: " + deckName, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), "input: " + deckName, Toast.LENGTH_SHORT).show();
 
         deckNameTitle.setText((CharSequence) deckName);
-}
+    }
 
+    public void goBack(){
+        Intent goBack = new Intent(this, AddDeck.class);
+        startActivity(goBack);
+    }
 
-
+    public void addCard(){
+        Intent addCard = new Intent(this, TypeChoice.class);
+        startActivity(addCard);
+    }
 
 
 
---
+++ app/src/main/java/com/example/secondtry/ui/decks/TypeChoice.java
@@ -0,0 +1,47 @@
+package com.example.secondtry.ui.decks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.secondtry.R;
+
+public class TypeChoice extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deck_create_add);
+
+        Button savebutton = (Button) findViewById(R.id.button_add_save);
+        savebutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                goBack();
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.question_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource((android.R.layout.simple_spinner_dropdown_item));
+        spinner.setAdapter(adapter);
+
+                        
+                
+
+
+    }
+
+    public void goBack(){
+        Intent goBack = new Intent(this, EditDeck.class);
+        startActivity(goBack);
+    }
+
+
+
+}
